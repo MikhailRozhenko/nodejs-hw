06da22ce2ecb1a44c66beda0af7b37d0f427119e
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-06-03 02:01:43 +0300

    Fix error handler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,15 @@
-export const errorHandler = (err, req, res, next) => {
+import { HttpError } from 'http-errors';
+
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
   res.status(500).json({
-    message: err.message,
+    message: err.message || 'Internal Server Error',
   });
 };
+
+export default errorHandler;
