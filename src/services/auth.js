802606cb1b87c6737a58b1fbadd028ed4550e857
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-06-22 15:13:02 +0300

    fix render

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,16 +1,12 @@
 import crypto from 'crypto';
-import Session from '../models/session.js';
+import { Session } from '../models/session.js';
 
 import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/time.js';
 
-/* ---------------- TOKEN GENERATION ---------------- */
+/* ---------------- TOKEN ---------------- */
+const generateToken = () => crypto.randomBytes(30).toString('hex');
 
-const generateToken = () => {
-  return crypto.randomBytes(30).toString('hex');
-};
-
-/* ---------------- SESSION CREATION ---------------- */
-
+/* ---------------- CREATE SESSION ---------------- */
 export const createSession = async (userId) => {
   const accessToken = generateToken();
   const refreshToken = generateToken();
@@ -18,20 +14,24 @@ export const createSession = async (userId) => {
   const accessTokenValidUntil = new Date(Date.now() + FIFTEEN_MINUTES);
   const refreshTokenValidUntil = new Date(Date.now() + ONE_DAY);
 
-  const session = await Session.create({
+  return await Session.create({
     userId,
     accessToken,
     refreshToken,
     accessTokenValidUntil,
     refreshTokenValidUntil,
   });
-
-  return session;
 };
 
-/* ---------------- COOKIE SETTER ---------------- */
-
+/* ---------------- COOKIES ---------------- */
 export const setSessionCookies = (res, session) => {
+  res.cookie('sessionId', session._id.toString(), {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    maxAge: ONE_DAY,
+  });
+
   res.cookie('accessToken', session.accessToken, {
     httpOnly: true,
     secure: true,
@@ -40,13 +40,6 @@ export const setSessionCookies = (res, session) => {
   });
 
   res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
-    maxAge: ONE_DAY,
-  });
-
-  res.cookie('sessionId', session._id.toString(), {
     httpOnly: true,
     secure: true,
     sameSite: 'none',
