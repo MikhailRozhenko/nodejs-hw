560e07a8e1e4f04bbffe4be074bccdba82aabe39
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-06-28 14:54:58 +0300

    fix render

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import * as userController from '../controllers/userController.js';
 import { authenticate } from '../middleware/authenticate.js';
 import { upload } from '../middleware/multer.js';
-import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
