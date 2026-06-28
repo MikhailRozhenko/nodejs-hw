9b7d03e8b8a68b6a4b41e17ab8e93364dabce784
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-06-28 15:17:32 +0300

    g

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,6 @@
 import crypto from 'crypto';
-import { Session } from '../models/session.js';
-
 import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/time.js';
+import Session from '../models/session.js';
 
 /* ---------------- TOKEN ---------------- */
 const generateToken = () => crypto.randomBytes(30).toString('hex');
