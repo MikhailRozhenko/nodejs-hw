61fac7c23337a5f17e21d4b18d67ca8915cde81a
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-06-28 15:23:20 +0300

    r

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,7 +3,7 @@ import createHttpError from 'http-errors';
 import jwt from 'jsonwebtoken';
 
 import Session from '../models/session.js';
-import { User } from '../models/user.js';
+import User from '../models/user.js';
 
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { sendEmail } from '../utils/sendMail.js';
