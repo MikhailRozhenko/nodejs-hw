180beee8439a7b7812b57771a66f8fd5e460921d
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-06-28 14:26:13 +0300

    fetch ren

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import * as userController from '../controllers/userController.js';
+import { authenticate } from '../middleware/authenticate.js';
 import { upload } from '../middleware/multer.js';
-import { authenticate } from '../middlewares/authenticate.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
